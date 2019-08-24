a7f95d8a939cc59d4aff1d9ccda53b4786cdd7f7
Vladimir Kondenko <kondenko2011@gmail.com>
2019-08-24 15:09:46 +0300

    Improve deletion result output

--- main.ts
+++ main.ts
@@ -1,6 +1,13 @@
 const selection = figma.currentPage.selection
 
-function isInstance(node, componentId) {
+const priorityDeleted = 1
+
+interface DeleteOperation {
+  message: string
+  priority: number
+}
+
+function isInstance(node: SceneNode, componentId: String) {
   try {
     return node && node.type == "INSTANCE" && node.masterComponent.id == componentId
   } catch (e) {
@@ -11,38 +18,54 @@ function isInstance(node, componentId) {
   }
 }
 
-function removeComponent(component) {
-  const messageTextPadding = "    "
+function removeComponent(component: SceneNode): DeleteOperation {
+  const messageTextPadding: string = "    "
   if (component != undefined && component.type == "COMPONENT" && !component.removed) {
-    const name = component.name
+    const name: string = component.name
     if (!component.remote) {
       const componentId = component.id
       const hasInstances = figma.root.findOne(node => isInstance(node, componentId)) != null
       if (!hasInstances) {
         component.remove()
-        return `🗑${messageTextPadding}${name} removed`
+        return { 
+          message: `🗑${messageTextPadding}${name} deleted`,
+          priority: 1 
+        }
       } else {
-        return `🔒${messageTextPadding}${name} has instances and won't be removed`
+        return { 
+          message: `🔒${messageTextPadding}${name} has instances and won't be deleted`,
+          priority: 2 
+        }
       }
     } else {
-      return `🚫${messageTextPadding}${name} is read-only`
+      return { 
+        message: `🚫${messageTextPadding}${name} is read-only`,
+        priority: 3 
+      }
     }
   } else {
-    return component ? `❓${messageTextPadding}${component.name} is not a component` : "No component selected"
+    return { 
+      message: component ? `❓${messageTextPadding}${component.name} is not a component` : "No component selected",
+      priority: 4 
+    }
   }
 }
 
-const messages = selection.map(node => removeComponent(node))
 
-if (messages.length < 4) {
-  messages.forEach(m => figma.notify(m))
+const messages: Array<DeleteOperation> = selection.map(node => removeComponent(node)).sort((x, y) => x.priority - y.priority)
+
+if (messages.find(res => res.priority == priorityDeleted) == undefined) {
+  figma.closePlugin("Nothing to delete")
+} else if (messages.length < 4) {
+  Object.values(messages).forEach(res => figma.notify(res.message))
   figma.closePlugin()
 } else {
-  const text = messages.join("\n\n")
+  const text = Object.values(messages).map(res => res.message).join("\n\n")
   const style =
   `
-  white-space:pre-wrap; 
-  padding: 0 8px;
+  white-space:pre-wrap;
+  padding: 8px;
+  line-height: 1.5;
   font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
   `
   figma.showUI(`<p style="${style}">${text}</p>`, { width: 500, height: 200 })
