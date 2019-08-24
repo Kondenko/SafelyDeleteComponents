4748f43b6baab2a11c9fd3b3a7fc0f778818e608
Vladimir Kondenko <kondenko2011@gmail.com>
2019-08-24 12:31:38 +0300

    Move component deletion into a separate function

--- main.ts
+++ main.ts
@@ -1,29 +1,42 @@
 const selection = figma.currentPage.selection
 
-var message
-
-if (selection.length == 1) {
-  const selectedNode = selection[0]
-  if (selectedNode != undefined && selectedNode.type == "COMPONENT" && !selectedNode.removed) {
-    if (!selectedNode.remote) {
-      const name = selectedNode.name
-      const componentId = selectedNode.id
-      const instancesCount = figma.root.findAll((node) => node.type == "INSTANCE" && node.masterComponent.id == componentId).length
-      const hasInstances = instancesCount > 0
-      if (!hasInstances) {
-        selectedNode.remove()
-        message = `${name} removed`
-      } else {
-        message = `${name} has ${instancesCount} instances and won't be removed`
-      }
-    } else {
-      message = `${name} is read-only`
-    }
-  } else {
-    message = selectedNode ? `${name} is not a component` : "No component selected"
+function isInstance(node, componentId) {
+  try {
+    return node && node.type == "INSTANCE" && node.masterComponent.id == componentId
+  } catch (e) {
+    console.log("Error traversing the document")
+    console.log(node)
+    console.log(e)
+    return false
   }
-} else {
-  message = "Select 1 object"
 }
 
-figma.closePlugin(message);
+function removeComponent(component) {
+  if (component != undefined && component.type == "COMPONENT" && !component.removed) {
+    const name = component.name
+    if (!component.remote) {
+      const componentId = component.id
+      const instances = figma.root.findOne(node => isInstance(node, componentId))
+      const hasInstances = instances != null
+      if (!hasInstances) {
+        component.remove()
+        figma.closePlugin(`${name} removed`)
+      } else {
+        figma.closePlugin(`${name} has instances and won't be removed`)
+      }
+    } else {
+      figma.closePlugin(`${name} is read-only`)
+    }
+  } else {
+    figma.closePlugin(component ? `${component.name} is not a component` : "No component selected")
+  }
+}
+
+// TODO Allow selecting multiple components
+if (selection.length == 1) {
+  const selectedNode = selection[0]
+  console.log(selectedNode)
+  removeComponent(selectedNode)
+} else {
+  figma.closePlugin(`Select 1 object (${selection.length} selected)`)
+}
